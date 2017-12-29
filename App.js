7b098c5e274cffba2c49e6bdc96dab8c69acf061
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2017-12-29 13:24:39 +0100

    advanced test

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import styled from 'styled-components/native';
 import { StyleSheet, Text, View } from 'react-native';
+
+const Rapallo = styled.Text`
+  font-size: 72px;
+  transform: rotate(180deg);
+`
+const TestWrap = styled.View`
+  width: 350px;
+  height: 230px;
+  border-radius: 4px;
+  border-left-color: #FFA309;
+  border-left-width: 5px;
+  background-color: #fff;
+  shadow-color: #000;
+  shadow-offset: 0px 7px;
+  shadow-radius: 3px;
+  shadow-opacity: 0.4;
+  margin: 34px 0 0 0;
+`
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>F A V A</Text>
+        <Text style={styles.text}>F A V A</Text>
+        <Rapallo>F A V A</Rapallo>
+      
+      <TestWrap>
+        
+      </TestWrap>
+
       </View>
     );
   }
@@ -14,8 +39,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'tomato',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 72,
+    color: '#fff', 
+
+  }
 });
+
